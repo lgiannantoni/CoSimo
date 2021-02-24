a2bd85e07521e765a598dad9fc9634374e9674e7
leonardogiannantoni <leonardo.giannantoni@gmail.com>
2021-02-24 16:33:05 +0100

    +__add__ and other Pipeline compatibility props

--- simulator.py
+++ simulator.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from collections import defaultdict
 
 from library.common.model import IModel
+from library.utils import Level
 
 
 class ISimulator(ABC):
@@ -16,9 +17,37 @@ class ISimulator(ABC):
     Methods
     -------
     """
+    level = Level.NOTSET
 
-    def __init__(self):
+    def __init__(self, _in: list, _remove: list, _add: list, name: str = ""):
         self.models = defaultdict()
+        self._input = set(_in)
+        self._remove = set(_remove)
+        self._add = set(_add)
+        self._name = name
+
+    def input_list(self) -> set:
+        return self._input
+
+    def remove_list(self) -> set:
+        return self._remove
+
+    def add_list(self) -> set:
+        return self._add
+
+    def __add__(self, other):
+        """To build the pipeline dynamically
+        Create a pipeline by adding two modules
+        Parameters
+        ----------
+        other : Module
+        """
+        if isinstance(other, ISimulator):
+            from library.common.pipeline import Pipeline
+            return Pipeline(self, other)
+        else:
+            raise TypeError("The second arg is not a {} object".format(ISimulator))
+
 
     @abstractmethod
     def add_model(self, *args, **kwargs):
