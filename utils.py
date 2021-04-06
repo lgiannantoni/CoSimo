e2fad7174d0848f9cc83c521c8a51469b7a9ed58
leonardogiannantoni <leonardo.giannantoni@gmail.com>
2021-04-06 18:08:17 +0200

    +Objectless class to prevent class instantiation (e.g. Shape class in geometry.py module)

--- utils.py
+++ utils.py
@@ -23,6 +23,10 @@ class Singleton(type):
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
+class Objectless:
+    def __new__(cls, *args, **kwargs):
+        raise RuntimeError('%s should not be instantiated' % cls)
+
 
 class AdvEnum(Enum):
     @classmethod
